0a48d6778125d320153bd1992290b3dec3fd7e38
Javier <jmontilva@faradaysec.com>
2020-01-21 09:28:47 -0400

    [ADD] Conditional to send output not read yet if the plugin is CSV

--- faraday_plugins/plugins/plugin.py
+++ faraday_plugins/plugins/plugin.py
@@ -166,7 +166,10 @@ class PluginBase:
 
     def _parse_filename(self, filename):
         with open(filename, **self.open_options) as output:
-            self.parseOutputString(output.read())
+            if self.id == "CSV":
+                self.parseOutputString(output)
+            else:
+                self.parseOutputString(output.read())
 
     def processReport(self, filepath, user="faraday"):
         if os.path.isfile(filepath):
