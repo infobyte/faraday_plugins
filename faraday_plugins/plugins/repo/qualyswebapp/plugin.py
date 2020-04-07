e8061a2c02daddc25d5f741d32946a83e2aaaf2a
Leonardo Lazzaro <leonardol@infobytesec.com>
2020-04-07 18:39:56 -0300

    Update qualys webapp plugin
    Use URL for creating hosts.
    Update severity map.
    Check for CVSS is not None.

--- faraday_plugins/plugins/repo/qualyswebapp/plugin.py
+++ faraday_plugins/plugins/repo/qualyswebapp/plugin.py
@@ -5,11 +5,12 @@ Faraday Penetration Test IDE
 Copyright (C) 2016  Infobyte LLC (http://www.infobytesec.com/)
 See the file 'doc/LICENSE' for the license information
 """
-from faraday_plugins.plugins.plugin import PluginXMLFormat
-import socket
-import random
 import re
-import os
+from urllib.parse import urlparse
+
+from dateutil.parser import parse
+
+from faraday_plugins.plugins.plugin import PluginXMLFormat
 
 try:
     import xml.etree.cElementTree as ET
@@ -109,11 +110,12 @@ class QualysWebappPlugin(PluginXMLFormat):
         self.options = None
         self._command_regex = re.compile(r'^(qualyswebapp |\.\/qualyswebapp).*?')
         self.protocol = None
-        self.hostname = None
         self.port = '80'
         self.address = None
 
     def parseOutputString(self, output):
+        hostnames = []
+
         parser = QualysWebappParser(output)
 
         if not parser.info_appendix:
@@ -124,38 +126,47 @@ class QualysWebappPlugin(PluginXMLFormat):
         for host_create in parser.info_appendix:
             self.scan_list_result.append(host_create)
 
-        ip_existe = 'SCANNER' in self.scan_list_result[0].lista_scan
-        if ip_existe:
-            self.ip = re.findall(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b",
-                                 self.scan_list_result[0].lista_scan['SCANNER'])
         self.credential = self.scan_list_result[0].lista_scan.get('AUTHENTICATION_RECORD')
-        self.scan_template = self.scan_list_result[0].lista_scan.get('NAME')
-        self.os = self.scan_list_result[1].lista_webapp.get('OPERATING_SYSTEM')
-        self.hostname = self.scan_list_result[1].lista_webapp.get('URL')
+        os = self.scan_list_result[1].lista_webapp.get('OPERATING_SYSTEM')
 
-        host_id = self.createAndAddHost(name=self.ip[0], os=[self.os], hostnames=[self.hostname], mac=None,
-                                        scan_template=self.scan_template)
+        if self.scan_list_result[1].lista_webapp.get('URL'):
+            initial_url = self.scan_list_result[1].lista_webapp.get('URL')
+            parsed_url = urlparse(initial_url)
+            hostnames = [parsed_url.netloc]
 
         glossary = []
         for glossary_qid in parser.info_glossary:
             glossary.append(glossary_qid.dict_result_qid)
 
         for v in parser.info_results:
+            url = urlparse(v.dict_result_vul.get('URL'))
+
+            host_id = self.createAndAddHost(name=url.netloc, os=os, hostnames=hostnames)
+
             vuln_scan_id = v.dict_result_vul.get('QID')
+
+            # Data in the xml is in different parts, we look into the glossary
             vuln_data = next((item for item in glossary if item["QID"] == vuln_scan_id), None)
             vuln_name = vuln_data.get('TITLE')
             vuln_desc = vuln_data.get('DESCRIPTION')
-            vuln_severity = vuln_data.get('SEVERITY')
-            vuln_since = v.dict_result_vul.get('FIRST_TIME_DETECTED')
+
+            raw_severity = int(vuln_data.get('SEVERITY', 0))
+            vuln_severity = raw_severity - 1
+
+            run_date = parse(v.dict_result_vul.get('FIRST_TIME_DETECTED'))
             vuln_resolution = vuln_data.get('SOLUTION')
-            vuln_ref = "CVSS: {}".format(vuln_data.get('CVSS_BASE'))
+
+            vuln_ref = []
+            if vuln_data.get('CVSS_BASE'):
+                vuln_ref = ["CVSS: {}".format(vuln_data.get('CVSS_BASE'))]
+
             vuln_data_add = "ID: {}, DETECTION_ID: {}, CATEGORY: {}, GROUP: {}, URL: {}, IMPACT: {}".format(
                 v.dict_result_vul.get('ID'), v.dict_result_vul.get('DETECTION_ID'), vuln_data.get('CATEGORY'),
                 vuln_data.get('GROUP'), v.dict_result_vul.get('URL'), vuln_data.get('IMPACT'))
 
             self.createAndAddVulnToHost(host_id=host_id, name=vuln_name, desc=vuln_desc, ref=vuln_ref,
-                                        severity=vuln_severity, resolution=vuln_resolution, vulnerable_since=vuln_since,
-                                        scan_id=vuln_scan_id, data=vuln_data_add)
+                                        severity=vuln_severity, resolution=vuln_resolution, run_date=run_date,
+                                        external_id=vuln_scan_id, data=vuln_data_add)
 
 
 def createPlugin():
