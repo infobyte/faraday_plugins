fd29bf628295ace1d7525237a1e14db5972325a8
Blas <bmoyano@infobytesec.com>
2020-04-11 14:47:09 -0300

    Fix port is None , fix logic for in vuln

--- faraday_plugins/plugins/repo/sourceclear/plugin.py
+++ faraday_plugins/plugins/repo/sourceclear/plugin.py
@@ -29,11 +29,13 @@ class SourceclearJsonParser:
         hostname = url_parse.netloc
         port = url_parse.port
 
-        if protocol == 'https':
-            port = 443
-        elif protocol == 'http':
-            if not port:
-                port = 80
+        if port is None:
+            if protocol == 'https':
+                port = 443
+            elif protocol == 'http':
+                if not port:
+                    port = 80
+
         return {'protocol': protocol, 'hostname': hostname, 'port': port}
 
 
@@ -52,22 +54,22 @@ class SourceclearPlugin(PluginJsonFormat):
 
     def parseOutputString(self, output, debug=False):
         parser = SourceclearJsonParser(output)
-        vulns = {}
+
         for records in parser.json_data['records']:
             vulns = records['vulnerabilities']
             h_id = self.createAndAddHost(name='0.0.0.0', scan_template=records['metadata']['recordType'])
 
-        for vuln in vulns:
-            v_name = vuln['title']
-            v_desc = vuln['overview']
-            v_ref = "CVSS: {}".format(vuln['cvssScore'])
-            v_data = vuln['libraries']
-            v_website = vuln['_links']['html']
-            url_data = parser.parse_url(v_website)
-            s_id = self.createAndAddServiceToHost(h_id, "Sourceclear", protocol=url_data['protocol'],
-                                                  ports=url_data['port'], status='open')
-            self.createAndAddVulnWebToService(h_id, s_id, name=v_name, desc=v_desc, ref=[v_ref], data=v_data,
-                                              website=v_website)
+            for vuln in vulns:
+                v_name = vuln['title']
+                v_desc = vuln['overview']
+                v_ref = "CVSS: {}".format(vuln['cvssScore'])
+                v_data = vuln['libraries']
+                v_website = vuln['_links']['html']
+                url_data = parser.parse_url(v_website)
+                s_id = self.createAndAddServiceToHost(h_id, "Sourceclear", protocol=url_data['protocol'],
+                                                      ports=url_data['port'], status='open')
+                self.createAndAddVulnWebToService(h_id, s_id, name=v_name, desc=v_desc, ref=[v_ref], data=v_data,
+                                                  website=v_website)
 
 
 def createPlugin():
