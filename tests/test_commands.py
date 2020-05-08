8fac6730247f51169d93dc09fa20cde85f9fe0fd
Nicolas Rebagliati <nicolas.rebagliati@aenima-x.com.ar>
2020-05-08 15:17:09 -0300

    delete unused code

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -3,28 +3,17 @@ import pytest
 from faraday_plugins.plugins.manager import PluginsManager, CommandAnalyzer
 from faraday_plugins.plugins.plugin import PluginBase
 
-BLACK_LIST = [
-    'LICENSE',
-    'README.md',
-    '.gitignore',
-    '.gitkeep',
-    'faraday_plugins_tests',
-
-]
 
 plugins_manager = PluginsManager()
 analyzer = CommandAnalyzer(plugins_manager)
 
 COMMANDS_FILE = './tests/commands.json'
 
-
 def list_commands():
     with open(COMMANDS_FILE) as f:
         commands_dict = json.load(f)
     for command_data in commands_dict["commands"]:
         yield command_data
-
-
 
 
 @pytest.mark.parametrize("command_data", list_commands())
