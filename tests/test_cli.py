159b7abbdf59d3b058f4871efd80a18ef21dd9a3
Nicolas Rebagliati <nicolas.rebagliati@aenima-x.com.ar>
2020-06-22 16:19:07 -0300

    add process_command tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,13 +1,14 @@
 import json
 import os
 import re
+from tempfile import NamedTemporaryFile
 from click.testing import CliRunner
-from faraday_plugins.commands import cli
+from faraday_plugins.commands import list_plugins, detect_command, process_command, detect_report, process_report
 
 
 def test_list_plugins():
     runner = CliRunner()
-    result = runner.invoke(cli, ['list-plugins'])
+    result = runner.invoke(list_plugins)
     assert result.exit_code == 0
     loaded_plugins = re.search(r'Loaded Plugins: (?P<loaded_plugins>\d+)', result.output)
     assert loaded_plugins
@@ -16,47 +17,59 @@ def test_list_plugins():
 
 def test_detect_invalid_command():
     runner = CliRunner()
-    result = runner.invoke(cli, ['detect-command', 'invalid_command'])
+    result = runner.invoke(detect_command, args=['invalid_command'])
     assert result.exit_code == 0
     assert result.output.strip() == "Failed to detect command: invalid_command"
 
 
 def test_detect_command():
     runner = CliRunner()
-    result = runner.invoke(cli, ['detect-command', 'ping -c 1 www.google.com'])
+    result = runner.invoke(detect_command, args=['ping -c 1 www.google.com'])
     assert result.exit_code == 0
     assert result.output.strip() == "Faraday Plugin: ping"
 
 
 def test_process_command():
     runner = CliRunner()
-    result = runner.invoke(cli, ['process-command', 'ping -c 1 www.google.com', '--summary'])
+    result = runner.invoke(process_command, args=['ping -c 1 www.google.com', '--summary'])
     assert result.exit_code == 0
     summary = json.loads(result.output.strip())
     assert summary['hosts'] == 1
 
 
+def test_process_command_to_file():
+    runner = CliRunner()
+    with runner.isolated_filesystem() as file_system:
+        output_file = os.path.join(file_system, "test.json")
+        result = runner.invoke(process_command, args=['ping -c 1 www.google.com',  '-o', output_file])
+        assert result.exit_code == 0
+        assert os.path.isfile(output_file)
+        with open(output_file) as f:
+            vuln_json = json.load(f)
+        assert len(vuln_json['hosts']) == 1
+
+
 def test_detect_report():
     report_file = os.path.join('./report-collection', 'faraday_plugins_tests', 'Nmap', 'nmap_5.21.xml')
     runner = CliRunner()
-    result = runner.invoke(cli, ['detect-report', report_file])
+    result = runner.invoke(detect_report, args=[report_file])
     assert result.exit_code == 0
     assert "Faraday Plugin: Nmap" == result.output.strip()
 
 
 def test_detect_report_dont_exists():
-    report_file = os.path.join('../report-collection', 'faraday_plugins_tests', 'Nmap', 'nmap_5.21.xml')
+    report_file = os.path.join('./report-collection', 'faraday_plugins_tests', 'Nmap', 'invalid_report.xml')
     runner = CliRunner()
-    result = runner.invoke(cli, ['detect-report', report_file])
+    result = runner.invoke(detect_report, args=[report_file])
     assert result.exit_code == 0
     assert "Don't Exists" in result.output.strip()
 
 
-def test_process_report():
+def test_process_report_summary():
     report_file = os.path.join('./report-collection', 'faraday_plugins_tests', 'Nmap', 'nmap_5.21.xml')
     summary_file = os.path.join('./report-collection', 'faraday_plugins_tests', 'Nmap', 'nmap_5.21_summary.json')
     runner = CliRunner()
-    result = runner.invoke(cli, ['process-report', report_file, '--summary'])
+    result = runner.invoke(process_report, args=[report_file, '--summary'])
     assert result.exit_code == 0
     summary = json.loads(result.output.strip())
     with open(summary_file) as f:
@@ -69,3 +82,4 @@ def test_process_report():
     assert summary['services_vulns'] == saved_summary['services_vulns']
     assert summary['severity_vulns'] == saved_summary['severity_vulns']
     assert vuln_hashes == saved_vuln_hashes
+
