096061f5f9254f51813a9fa347c6e66f168f4cd9
Blas <bmoyano@infobytesec.com>
2020-09-20 23:10:02 -0300

    UPD parser info in MBSA

--- faraday_plugins/plugins/repo/mbsa/plugin.py
+++ faraday_plugins/plugins/repo/mbsa/plugin.py
@@ -6,6 +6,7 @@ See the file 'doc/LICENSE' for the license information
 from faraday_plugins.plugins.plugin import PluginByExtension
 import re
 import os
+from datetime import datetime
 
 __author__ = "Blas Moyano"
 __copyright__ = "Copyright (c) 2020, Infobyte LLC"
@@ -17,11 +18,11 @@ __status__ = "Development"
 
 
 class MbsaParser:
-    def __init__(self, xml_output):
-        self.tree = self.parse_xml(xml_output)
+    def __init__(self, log_output):
+        pass
 
 
-class MaltegoPlugin(PluginByExtension):
+class MbsaPlugin(PluginByExtension):
     def __init__(self):
         super().__init__()
         self.id = "MBSA"
@@ -32,10 +33,66 @@ class MaltegoPlugin(PluginByExtension):
         self.extension = ".log"
 
     def parseOutputString(self, output):
-        print(type(output.find()))
 
-        #parser = MbsaParser(output)
+        computer_name = re.search('(Computer name:) (.*[A-Z])', output)
+        ip = re.search('(IP address:) ([0-9]+(?:\.[0-9]+){3})', output)
+        scan_date = re.search('(Scan date:) (.*[0-9])', output)
+        issues = re.findall(r'Issue: .*', output)
+        score = re.findall(r'Score: .*', output)
+        result = re.findall(r'Result: .*', output)
+        detail = ''
+        i = 0
+        issues_top = len(issues)
+
+        host_id = self.createAndAddHost(
+            ip.group(2),
+            'Windows',
+            hostnames=[computer_name.group(2)])
+
+        for issue in issues:
+
+            test = re.search(issues[i], output)
+
+            if i+1 != issues_top:
+                test_issue = re.search(issues[i+1], output)
+            else:
+                end = None
+            try:
+                start = test.end()
+                end = test_issue.start()
+            except:
+                start = None
+
+            if start is not None:
+                if end is None:
+                    result_info = output[start:]
+                else:
+                    result_info = output[start:end]
+                    result_info.rstrip('\n')
+                    result_info = result_info.replace(score[i], '')
+                    result_info = result_info.replace(result[i], '')
+                    result_info = result_info.strip()
+                    if result_info:
+                        print("ntro")
+                        detail = re.search('(Detail:)', result_info)
+                        if not None:
+                            detail = result_info
+                            result_info = result[i]
+
+                    else:
+                        detail = ''
+                        result_info = result[i]
+
+            self.createAndAddVulnToHost(host_id,
+                                        issue.replace('Issue: ', ''),
+                                        desc=score[i].replace('Score: ', ''),
+                                        ref=None,
+                                        resolution=result_info.replace('Result: ', ''),
+                                        data=detail,
+                                        run_date=datetime.strptime(scan_date.group(2), '%Y/%m/%d %H:%M'))
+
+            i += 1
 
 
 def createPlugin():
-    return MaltegoPlugin()
+    return MbsaPlugin()
