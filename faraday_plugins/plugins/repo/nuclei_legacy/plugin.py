f8e91ef83cc3a79eb5b2217f332707508003760c
Emilio <ecouto@faradaysec.com>
2021-11-16 21:44:36 +0900

    add plugin nuclei_legacy to support 2.5.2 JSON

--- faraday_plugins/plugins/repo/nuclei_legacy/plugin.py
+++ faraday_plugins/plugins/repo/nuclei_legacy/plugin.py
@@ -1,42 +1,32 @@
-"""
-Faraday Penetration Test IDE
-Copyright (C) 2020  Infobyte LLC (http://www.infobytesec.com/)
-See the file 'doc/LICENSE' for the license information
-
-"""
-import socket
 import re
 import json
 import dateutil
-from collections import defaultdict
 from urllib.parse import urlparse
 from faraday_plugins.plugins.plugin import PluginMultiLineJsonFormat
 from faraday_plugins.plugins.plugins_utils import resolve_hostname
 
-__author__ = "Nicolas Rebagliati"
-__copyright__ = "Copyright (c) 2021, Infobyte LLC"
-__credits__ = ["Nicolas Rebagliati"]
+__author__ = "Emilio Couto"
+__copyright__ = "Copyright (c) 2021, Faraday Security"
+__credits__ = ["Emilio Couto"]
 __license__ = ""
 __version__ = "1.0.0"
-__maintainer__ = "Nicolas Rebagliati"
-__email__ = "nrebagliati@infobytesec.com"
+__maintainer__ = "Emilio Couto"
+__email__ = "ecouto@infobytesec.com"
 __status__ = "Development"
 
 
-class NucleiPlugin(PluginMultiLineJsonFormat):
+class NucleiLegacyPlugin(PluginMultiLineJsonFormat):
     """ Handle the Nuclei tool. Detects the output of the tool
     and adds the information to Faraday.
     """
 
     def __init__(self, *arg, **kwargs):
         super().__init__(*arg, **kwargs)
-        self.id = "nuclei"
-        self.name = "Nuclei"
-        self.plugin_version = "1.0.2"
-        self.version = "2.5.3"
-        self.json_keys = {"matched-at", "template-id", "host"}
-        self._command_regex = re.compile(r'^(sudo nuclei|nuclei|\.\/nuclei|^.*?nuclei)\s+.*?')
-        self.xml_arg_re = re.compile(r"^.*(-o\s*[^\s]+).*$")
+        self.id = "nuclei_legacy"
+        self.name = "Nuclei < 2.5.3"
+        self.plugin_version = "1.0.0"
+        self.version = "2.5.2"
+        self.json_keys = {"matched", "templateID", "host"}
         self._use_temp_file = True
         self._temp_file_extension = "json"
 
@@ -63,7 +53,7 @@ class NucleiPlugin(PluginMultiLineJsonFormat):
                 status='open',
                 version='',
                 description='web server')
-            matched = vuln_dict.get('matched-at')
+            matched = vuln_dict.get('matched')
             matched_data = urlparse(matched)
             reference = vuln_dict["info"].get('reference', [])
             if not reference:
@@ -97,10 +87,9 @@ class NucleiPlugin(PluginMultiLineJsonFormat):
                 method = request.split(" ")[0]
             else:
                 method = ""
-
-            data = [f"Matched: {vuln_dict.get('matched-at')}",
+            data = [f"Matched: {vuln_dict.get('matched', '')}",
                     f"Tags: {vuln_dict['info'].get('tags', '')}",
-                    f"Template ID: {vuln_dict.get('template-id', '')}"]
+                    f"Template ID: {vuln_dict.get('templateID', '')}"]
 
             name = vuln_dict["info"].get("name")
             run_date = vuln_dict.get('timestamp')
@@ -125,28 +114,9 @@ class NucleiPlugin(PluginMultiLineJsonFormat):
                 params=matched_data.params,
                 path=matched_data.path,
                 data="\n".join(data),
-                external_id=f"NUCLEI-{vuln_dict.get('template-id', '')}",
+                external_id=f"NUCLEI-{vuln_dict.get('templateID', '')}",
                 run_date=run_date
             )
 
-    def processCommandString(self, username, current_path, command_string):
-        """
-        Adds the -oX parameter to get xml output to the command string that the
-        user has set.
-        """
-        super().processCommandString(username, current_path, command_string)
-        arg_match = self.xml_arg_re.match(command_string)
-        if arg_match is None:
-            return re.sub(r"(^.*?nuclei)",
-                          r"\1 --json -irr -o %s" % self._output_file_path,
-                          command_string)
-        else:
-            return re.sub(arg_match.group(1),
-                          r"--json -irr -o %s" % self._output_file_path,
-                          command_string)
-
-
 def createPlugin(ignore_info=False):
-    return NucleiPlugin(ignore_info=ignore_info)
-
-
+    return NucleiLegacyPlugin(ignore_info=ignore_info)
