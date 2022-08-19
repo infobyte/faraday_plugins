e0b4ec3fe802edb81099b73f537bbf95a6231a10
GMartinez1995 <gmartinez@infobytesec.com>
2022-08-19 17:03:40 -0300

    Fix when host is ip

--- faraday_plugins/plugins/repo/acunetix_json/plugin.py
+++ faraday_plugins/plugins/repo/acunetix_json/plugin.py
@@ -5,6 +5,7 @@ See the file 'doc/LICENSE' for the license information
 
 """
 from urllib.parse import urlsplit
+import ipaddress
 
 from lxml import etree
 
@@ -92,6 +93,8 @@ class AcunetixJsonPlugin(PluginJsonFormat):
 
     def new_structure(self, site: Scan):
         start_url = site.info.host
+        if site.info.start_url:
+            start_url = site.info.start_url
         url_data = urlsplit(start_url)
         site_ip = self.resolve_hostname(url_data.hostname)
         ports = '443' if (url_data.scheme == 'https') else '80'
@@ -128,7 +131,6 @@ class AcunetixJsonPlugin(PluginJsonFormat):
         url_data = urlsplit(url)
         if not url_data.scheme:
             url_data = urlsplit(scan.crawler.start_url_attr)
-        return url_data
 
 
 def createPlugin(ignore_info=False, hostname_resolution=True):
