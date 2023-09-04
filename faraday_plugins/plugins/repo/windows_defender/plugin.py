156358396b1a66bf865820116102f04fdb466f28
Esteban Rodriguez <erodriguez@infobytesec.com>
2023-09-04 00:29:54 -0300

    Creacion Windows defender

--- faraday_plugins/plugins/repo/windows_defender/plugin.py
+++ faraday_plugins/plugins/repo/windows_defender/plugin.py
@@ -1,80 +1,61 @@
 import json
 from faraday_plugins.plugins.plugin import PluginJsonFormat
 
-__author__ = "Esteban Rodriguez"
-__copyright__ = "Copyright (c) 2013, Infobyte LLC"
-__credits__ = ["Esteban Rodriguez"]
-__license__ = ""
-__version__ = "1.0.0"
-__maintainer__ = "Esteban Rodriguez"
-__email__ = "erodriguez@infobytesec.com"
-__status__ = "Development"
-
-
-class WindowsDefenderJsonParser:
-
-    def __init__(self, json_output):
-        self.data = json.loads(json_output)
-
-    def parse(self):
-        result = []
-
-        for entry in self.data:
-            device_name = entry.get('DeviceName')
-            os_platform = entry.get('OSPlatform')
-            cve_id = entry.get('CveId')
-            severity = entry.get('VulnerabilitySeverityLevel')
-
-            # Build the vulnerability description including all fields
-            description = f"Device Name: {device_name}\n"
-            description += f"OS Platform: {os_platform}\n"
-            description += f"CVE ID: {cve_id}\n"
-            description += f"Vulnerability Severity Level: {severity}\n"
-
-            result.append({
-                "host_info": {
-                    "name": "Windows Host",
-                    "os": os_platform,
-                    "hostname": device_name
-                },
-                "vulnerability": {
-                    "name": cve_id,
-                    "severity": severity,
-                    "desc": description
-                }
-            })
-
-        return result
-
 class WindowsDefenderPlugin(PluginJsonFormat):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.id = "WindowsDefender_Json"
+        self.id = "WindowsDefender_JSON"
         self.name = "Windows Defender Json"
         self.plugin_version = "1.0"
         self.version = "1.0"
-        self.json_keys = {'RbacGroupName'}
+        self.json_keys = {'LastSeenTimestamp' , 'SecurityUpdateAvailable'}  # Define the relevant JSON keys here
+        self._command_regex = None
+        self.json_arg_re = None
+        self._use_temp_file = False
+        self._temp_file_extension = None
 
     def parseOutputString(self, output):
-        parser = WindowsDefenderJsonParser(output)
-        parsed_data = parser.parse()
+        try:
+            data_list = output.strip().split('\n')  # Split the input into lines
 
-        for entry in parsed_data:
-            host_info = entry.get("host_info")
-            vulnerability = entry.get("vulnerability")
+            for json_str in data_list:
+                data = json.loads(json_str, object_hook=dict)  # Force conversion to a dictionary
 
-            host_id = self.createAndAddHost(
-                host_info.get("hostname"),
-                os=host_info.get("os"),
-                hostnames=[host_info.get("hostname")]
-            )
+                device_name = data.get('DeviceName', 'Unknown')
+                os_platform = data.get('OSPlatform', 'Unknown')
+                cve_id = data.get('CveId', 'Unknown')
+                severity = data.get('VulnerabilitySeverityLevel', 'Unknown')
+                DeviceId = data.get('DeviceId', 'Unknown')
+                key_value_pairs = "\n".join([f"{key}: {value}" for key, value in data.items()])
 
-            self.createAndAddVulnToHost(host_id,
-                name=vulnerability.get("name"),
-                severity=vulnerability.get("severity"),
-                desc=vulnerability.get("desc")
-            )
+
+                # Build the vulnerability description including all fields
+                description = f"Device Name: {device_name}\n "
+                description += f"OS Platform: {os_platform}\n "
+                description += f"CVE ID: {cve_id}\n "
+                description += f"Vulnerability Severity Level: {severity}\n "
+                description += f"Device ID: {severity}\n "
+                description += key_value_pairs
+
+
+                host_id = self.createAndAddHost(
+                    name=device_name,
+                    os=os_platform,
+                    hostnames=[device_name]
+                )
+
+                self.createAndAddVulnToHost(
+                    host_id,
+                    name=cve_id,
+                    severity=severity,
+                    desc=description
+                )
+
+        except json.JSONDecodeError as e:
+            self.logger.error(f"Error decoding JSON data: {str(e)}")
+        except Exception as e:
+            self.logger.error(f"Error parsing JSON data: {str(e)}")
 
 def createPlugin(*args, **kwargs):
     return WindowsDefenderPlugin(*args, **kwargs)
