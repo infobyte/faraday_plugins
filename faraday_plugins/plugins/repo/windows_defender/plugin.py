0575a1349b2cc75fcfce4db76cf2a6c275922bab
GMartinez1995 <gmartinez@infobytesec.com>
2023-09-04 16:29:08 -0300

    minor fixes

--- faraday_plugins/plugins/repo/windows_defender/plugin.py
+++ faraday_plugins/plugins/repo/windows_defender/plugin.py
@@ -7,59 +7,46 @@ class WindowsDefenderPlugin(PluginMultiLineJsonFormat):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.id = "WindowsDefender_JSON"
-        self.name = "Windows Defender Json"
+        self.id = "WindowsDefender_JSONL"
+        self.name = "Windows Defender Jsonl"
         self.plugin_version = "1.0"
         self.version = "1.0"
         self.json_keys = {'LastSeenTimestamp' , 'SecurityUpdateAvailable'}
 
 
-
-
     def parseOutputString(self, output):
-        try:
-            data_list = output.strip().split('\n')  # Split the input into lines
+        for json_str in filter(lambda x: x != '', output.split("\n")):
+            data = json.loads(json_str)
 
-            for json_str in data_list:
-                data = json.loads(json_str, object_hook=dict)  # Force conversion to a dictionary
+            device_name = data.pop('DeviceName', 'Unknown')
+            os_platform = data.pop('OSPlatform', 'Unknown')
+            cve_id = data.pop('CveId', 'Unknown')
+            severity = data.pop('VulnerabilitySeverityLevel', 'Unknown')
+            device_id = data.pop('DeviceId', 'Unknown')
+            software_name = data.pop('SoftwareName', 'Unknown')
+            software_vendor = data.pop('SoftwareVendor', 'Unknown')
+            data.pop('CvssScore')
+            key_value_pairs = "\n".join([f"{key}: {value}" for key, value in data.items()])
 
-                device_name = data.get('DeviceName', 'Unknown')
-                os_platform = data.get('OSPlatform', 'Unknown')
-                cve_id = data.get('CveId', 'Unknown')
-                severity = data.get('VulnerabilitySeverityLevel', 'Unknown')
-                device_id = data.get('DeviceId', 'Unknown')
-                software_name = data.get('SoftwareName', 'Unknown')
-                software_vendor = data.get('SoftwareVendor', 'Unknown')
+            # Build the vulnerability description including all fields
+            description = f"Device Name: {device_name}\n "\
+                          f"Device ID: {device_id}\n "\
+                          f"OS Platform: {os_platform}\n "\
+                          f"Misc Data of the vulnerability: {key_value_pairs}\n "
 
-                key_value_pairs = "\n".join([f"{key}: {value}\n" for key, value in data.items()])
+            host_id = self.createAndAddHost(
+                name=device_name,
+                os=os_platform,
+                hostnames=[device_name]
+            )
 
-
-                # Build the vulnerability description including all fields
-                description = f"Device Name: {device_name}\n "
-                description += f"Device ID: {device_id}\n "
-                description += f"OS Platform: {os_platform}\n "
-                description += f"Vulnerability Severity Level: {severity}\n "
-                description += f"Severity: {severity}\n "
-                description += f"Misc Data of the vulnerability: {key_value_pairs}\n "
-
-
-                host_id = self.createAndAddHost(
-                    name=device_name,
-                    os=os_platform,
-                    hostnames=[device_name]
-                )
-
-                self.createAndAddVulnToHost(
-                    host_id,
-                    name= f"{software_name}  {software_vendor} Vulnerable",
-                    cve=cve_id,
-                    severity=severity,
-                    desc=description
-                )
-
-        except json.JSONDecodeError as e:
-            self.logger.error(f"Error decoding JSON data: {str(e)}")
-
+            self.createAndAddVulnToHost(
+                host_id,
+                name= f"{software_name}  {software_vendor} Vulnerable",
+                cve=cve_id,
+                severity=severity,
+                desc=description
+            )
 
 def createPlugin(*args, **kwargs):
     return WindowsDefenderPlugin(*args, **kwargs)
