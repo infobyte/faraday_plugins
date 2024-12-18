459ac45f6cc11838e867f4709f73ab19565c457b
Diego Nadares <dnadares@faradaysec.com>
2024-12-18 12:08:44 -0300

    Add tests

--- tests/test_saint.py
+++ tests/test_saint.py
@@ -49,9 +49,13 @@ def test_all_fields():
 
     assert set(["127.0.0.1", "127.0.0.2", "127.0.0.3"]) == {host['ip'] for host in plugin_json['hosts']}
 
-    assert "127.0.0.1" in get_host("127.0.0.1", plugin_json['hosts']).get("hostnames")
-    assert "127.0.0.1" in get_host("127.0.0.2", plugin_json['hosts']).get("hostnames")
+    assert len(get_host("127.0.0.1", plugin_json['hosts']).get("hostnames")) == 2
+    assert len(get_host("127.0.0.2", plugin_json['hosts']).get("hostnames")) == 1
     assert len(get_host("127.0.0.3", plugin_json['hosts']).get("hostnames")) == 0
+
+    assert "127.0.0.1" in get_host("127.0.0.1", plugin_json['hosts']).get("hostnames")
+    assert "127.0.0.2" in get_host("127.0.0.1", plugin_json['hosts']).get("hostnames")
+    assert "127.0.0.1" in get_host("127.0.0.2", plugin_json['hosts']).get("hostnames")
 
     assert len(get_host("127.0.0.1", plugin_json['hosts']).get("vulnerabilities", [])) == 2
     assert len(get_host("127.0.0.2", plugin_json['hosts']).get("vulnerabilities", [])) == 1
