25a26585a71ce3c034aa48edd975c93d25fe2522
Pablo <pablop@faradaysec.com>
2025-03-25 13:57:03 -0300

    Fix copyright

--- faraday_plugins/plugins/repo/tenableio_export_json/plugin.py
+++ faraday_plugins/plugins/repo/tenableio_export_json/plugin.py
@@ -1,6 +1,6 @@
 """
 Faraday Penetration Test IDE
-Copyright (C) 2025  Infobyte LLC (http://www.infobytesec.com/)
+Copyright (C) 2025  Faraday LLC (http://www.faradaysec.com/)
 See the file 'doc/LICENSE' for the license information
 
 """
