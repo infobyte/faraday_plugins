b2b05b8a590033e1b1cdfd2df54f2f21b5c408e9
erodriguez11 <erodriguez@faradaysec.com>
2025-05-21 14:35:32 -0300

    fix black and flake8

--- faraday_plugins/plugins/repo/securityscoreboard_csv/plugin.py
+++ faraday_plugins/plugins/repo/securityscoreboard_csv/plugin.py
@@ -6,7 +6,7 @@ See the file 'doc/LICENSE' for the license information
 
 from faraday_plugins.plugins.plugin import PluginCSVFormat
 import pandas as pd
-from  io import StringIO
+from io import StringIO
 
 __author__ = "Erodriguez"
 __copyright__ = "Copyright (c) 2019, Infobyte LLC"
@@ -25,7 +25,7 @@ class SecScoreBoard_CSV(PluginCSVFormat):
 
     def __init__(self, *arg, **kwargs):
         super().__init__(*arg, **kwargs)
-        self.csv_headers = [{'LABEL'},{'PROVIDER'}]
+        self.csv_headers = [{"LABEL"}, {"PROVIDER"}]
         self.id = "SecScoreBoard_CSV"
         self.name = "SecScoreBoard_CSV Output Plugin"
         self.plugin_version = "0.0.1"
@@ -40,7 +40,11 @@ class SecScoreBoard_CSV(PluginCSVFormat):
             # Iterate through DataFrame rows
             for _, row in df.iterrows():
                 # Get path from different possible columns
-                path = row.get("FINAL URL", "") or row.get("IP ADDRESS", "") or row.get("HOSTNAME", "")
+                path = (
+                    row.get("FINAL URL", "")
+                    or row.get("IP ADDRESS", "")
+                    or row.get("HOSTNAME", "")
+                )
 
                 name = row.get("ISSUE TYPE TITLE", "")
 
@@ -50,7 +54,7 @@ class SecScoreBoard_CSV(PluginCSVFormat):
                     references.append(row["CVE"])
 
                 # Handle description
-                desc = str(row.get('DESCRIPTION', ''))
+                desc = str(row.get("DESCRIPTION", ""))
 
                 # Create host and vulnerability
                 h_id = self.createAndAddHost(name=path)
@@ -58,12 +62,12 @@ class SecScoreBoard_CSV(PluginCSVFormat):
                     h_id,
                     name=name,
                     desc=desc,
-                    resolution=str(row.get('ISSUE RECOMMENDATION', '')),
-                    external_id=str(row.get('ISSUE ID', '')),
-                    cve=str(row.get('CVE', '')),
+                    resolution=str(row.get("ISSUE RECOMMENDATION", "")),
+                    external_id=str(row.get("ISSUE ID", "")),
+                    cve=str(row.get("CVE", "")),
                     severity=str(row.get("ISSUE TYPE SEVERITY", "")),
                     ref=references,
-                    data=str(row.get("DATA", ""))
+                    data=str(row.get("DATA", "")),
                 )
 
         except Exception as e:
