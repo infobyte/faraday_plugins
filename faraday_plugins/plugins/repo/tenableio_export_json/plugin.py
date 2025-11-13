71c0b0581779cf7080455bf3c8b1cccbc937a2ef
Diego Nadares <dnadares@faradaysec.com>
2025-11-13 15:44:52 -0300

    Fix list access

--- faraday_plugins/plugins/repo/tenableio_export_json/plugin.py
+++ faraday_plugins/plugins/repo/tenableio_export_json/plugin.py
@@ -62,7 +62,7 @@ class TenableIOJSONExport(PluginJsonFormat):
     def get_hostname_from_host(self, ip: str) -> str:
         for host in self.vulns_data["hosts"]:
             if ip in host.get("ip", []):
-                return host.get("hostnames", "")[0]
+                return host.get("hostnames", [""])[0]
 
     def parseOutputString(self, output: str) -> None:
         try:
